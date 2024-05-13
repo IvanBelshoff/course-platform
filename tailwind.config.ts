882742086cd19486a24819106d06dbe0f2f64a3b
IvanBelshoff <ivanbelshoff@gmail.com>
2024-05-13 09:49:25 -0300

    feat: add tailwind css extended theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
+import { error } from "console";
+import { text } from "stream/consumers";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        text: 'var(--color-text)',
+        error: 'var(--color-error)',
+        paper: 'var(--color-paper)',
+        primary: 'var(--color-primary)',
+        background: 'var(--color-background)',
+        'primary-contrast': 'var(--color-primary-contrast)'
+      }
+    },
   },
   plugins: [],
 };
