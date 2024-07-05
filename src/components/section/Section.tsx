4e26357ec2cfc4641aa61bb91b2d20ca151a9f94
ivanstriker13 <ivanbelshoff@gmail.com>
2024-07-05 17:08:57 -0300

    feat: improve sections button and scroll

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,4 +1,7 @@
+'use client';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import { Card, IcardProps } from "../card/Card";
+import { UIEvent, useRef, useState } from "react";
 
 interface ISectionProps {
     title: string;
@@ -7,6 +10,28 @@ interface ISectionProps {
 }
 
 export const Section = ({ title, variant = 'grid', items }: ISectionProps) => {
+
+    const scrollRef = useRef<HTMLUListElement>(null);
+
+    const [scrollAlt, setScrollAt] = useState<'start' | 'middle' | 'end'>('start');
+
+    const handleScroll = (event: UIEvent<HTMLUListElement, globalThis.UIEvent>) => {
+        if (event.currentTarget.scrollLeft === 0) {
+            setScrollAt('start');
+        } else if ((event.currentTarget.scrollWidth - event.currentTarget.clientWidth) === event.currentTarget.scrollLeft) {
+            setScrollAt('end');
+        } else {
+            setScrollAt('middle');
+        }
+    };
+
+    const handleSetScroll = (scroll: number) => {
+        const currentScrollLeft = scrollRef.current?.scrollLeft || 0;
+
+        scrollRef.current?.scrollTo({ behavior: 'smooth', left: currentScrollLeft + scroll });
+    };
+
+
     return (
         <section className='flex flex-col gap-4 px-4'>
             <h2 className='font-bold text-xl'>
@@ -14,8 +39,20 @@ export const Section = ({ title, variant = 'grid', items }: ISectionProps) => {
             </h2>
 
             <ul
+                ref={scrollRef}
                 data-variant={variant}
-                className='grid gap-2 grid-cols-1 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto'>
+                onScroll={handleScroll}
+                className='grid gap-2 grid-cols-1 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto'
+            >
+
+                {variant === 'h-list' && (
+                    <button
+                        disabled={scrollAlt == 'start'}
+                        onClick={() => handleSetScroll(-350)}
+                        className='h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto left-0 -ml-14 transition-opacity disabled:opacity-0 active:opacity-80'>
+                        <MdKeyboardArrowLeft size={32} />
+                    </button>
+                )}
 
                 {items.map((card) => (
                     <li key={card.title} data-variant={variant} className='w-full data-[variant=h-list]:sm:w-72'>
@@ -28,6 +65,14 @@ export const Section = ({ title, variant = 'grid', items }: ISectionProps) => {
                     </li>
                 ))}
 
+                {variant === 'h-list' && (
+                    <button
+                        disabled={scrollAlt == 'end'}
+                        onClick={() => handleSetScroll(350)}
+                        className='h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto right-0 -ml-14 transition-opacity disabled:opacity-0 active:opacity-80'>
+                        <MdKeyboardArrowRight size={32} />
+                    </button>
+                )}
 
             </ul>
         </section>
