36829b7e3bbd57cd5647701d8c55d9cd58a5f74d
ivanstriker13 <ivanbelshoff@gmail.com>
2024-08-20 11:00:25 -0300

    feat: add class group component

--- src/app/cursos/[id]/page.tsx
+++ src/app/cursos/[id]/page.tsx
@@ -3,14 +3,16 @@ import { Metadata } from 'next';
 
 import { CourseHeader } from '@/components/couser-header/CourseHeader';
 import { StartCourse } from '@/components/StartCourse';
-import { Class } from '@/components/course-content/components/Class';
+import { ClassGroup } from '@/components/course-content/components/ClassGroup';
 
 interface Props {
     params: { id: string }
 }
+
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
     return {
-        title: "CodarSe - Curso de API Rest, Node e Typescript",
+        title: params.id,
+        description: params.id
     };
 };
 
@@ -19,15 +21,22 @@ export default function pageCourseDetail({ params }: Props) {
         <main className='mt-8 flex justify-center'>
             <div className='w-full min-[880px]:max-w-[880px] px-2 lg:px-0 flex flex-col md:flex-row-reverse gap-4'>
                 <div className='flex-1'>
-                    <StartCourse title='Curso de Figma para Devs' idClass='1' idCouser='1' imageUri='https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg' />
-
+                    <StartCourse
+                        title='Curso de Figma para Devs'
+                        idClass='1'
+                        idCouser='1'
+                        imageUri='https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg' />
                 </div>
-                <div className='flex-[2]'>
+                <div className='flex-[2] flex flex-col gap-4'>
                     <CourseHeader />
-                    <Class
-                        playerUrl={`/player/{courseId}/{classId}`}
-                        title='Figma para DEVs: #00 - Apresentação do curso'
-                    />
+                    <ClassGroup
+                        title={'Introdução e apresentação do projeto'}
+                        courseId={'123'}
+                        classes={[
+                            { id: '1', title: 'Figma para DEVs: #00 - Apresentação do curso' },
+                            { id: '2', title: 'Figma para DEVs: #01 - Apresentação do curso' },
+                            { id: '3', title: 'Figma para DEVs: #02 - Apresentação do curso' },
+                        ]} />
                 </div>
             </div>
         </main>
---
+++ src/components/course-content/components/ClassGroup.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useState } from "react";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+
+import { Class } from "./Class";
+
+interface IClassGroupProps {
+    title: string;
+    courseId: string;
+    classes: Array<{ id: string, title: string }>
+}
+
+export const ClassGroup: React.FC<IClassGroupProps> = ({ classes, courseId, title }) => {
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <button
+                className='flex items-center gap-6 p-4 bg-paper rounded'
+                onClick={() => setOpen(!open)}
+            >
+                {open ? (<MdKeyboardArrowDown size={24} />) : (<MdKeyboardArrowUp size={24} />)}
+                {title}
+            </button>
+
+            <ol data-open={open} className='flex flex-col data-[open=false]:hidden'>
+                {classes && classes.map(({ id, title }) => (
+                    <li key={id}>
+                        <Class title={title} playerUrl={`/player/${courseId}/${id}`} />
+                    </li>
+                ))}
+            </ol>
+
+        </>
+    );
+};
