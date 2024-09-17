934e02ad0b01ae8cca97098fdbe3333bc4a21ebc
ivanstriker13 <ivanbelshoff@gmail.com>
2024-09-17 16:52:10 -0300

    feat: add tabs components

--- src/components/player/player-class-details/PlayerClassDetails.tsx
+++ src/components/player/player-class-details/PlayerClassDetails.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from 'next/navigation';
 import React, { useMemo } from 'react';
 import { IPlayerClassGroupProps, PlayerVideoPlayer } from '..';
+import * as Tabs from '@radix-ui/react-tabs';
 
 interface IPlayerClassDetailsProps {
     playingCourseId: string;
@@ -22,7 +23,7 @@ export const PlayerClassDetails: React.FC<IPlayerClassDetailsProps> = ({ playing
 
         const nextClassIndex = currentClassIndex + 1;
 
-        if(nextClassIndex === classes.length) {
+        if (nextClassIndex === classes.length) {
             return undefined;
         }
 
@@ -33,15 +34,43 @@ export const PlayerClassDetails: React.FC<IPlayerClassDetailsProps> = ({ playing
 
     return (
         <div className='flex-1'>
+
             <div className='aspect-video'>
                 <PlayerVideoPlayer
                     videoId={'bP47qRVRqQs'}
                     onPlayNext={() => nextClassId ? router.push(`/player/${playingCourseId}/${nextClassId}`) : undefined}
                 />
             </div>
-            <div>
-                Descrição
-            </div>
+
+            <Tabs.Root defaultValue='class-details'>
+                <Tabs.List className='flex gap-4 '>
+                    <Tabs.Trigger
+                        value='class-details'
+                        className='p-2 flex items-center justify-center border-b-4 border-transparent data-[state=active]:border-primary'
+                    >Conteúdo do Curso</Tabs.Trigger>
+                    <Tabs.Trigger
+                        value='class-comments'
+                        className='p-2 flex items-center justify-center border-b-4 border-transparent data-[state=active]:border-primary'
+                    >Comentários</Tabs.Trigger>
+                    <Tabs.Trigger
+                        value='course-details'
+                        className='p-2 flex items-center justify-center border-b-4 border-transparent data-[state=active]:border-primary'
+                    >Visão geral do curso</Tabs.Trigger>
+                </Tabs.List>
+
+                <hr className='border-paper' />
+
+                <Tabs.Content value='class-details'>
+                    Detalhes da aula
+                </Tabs.Content>
+                <Tabs.Content value='class-comments'>
+                    Comentários da aula
+                </Tabs.Content>
+                <Tabs.Content value='course-details'>
+                    Detalhes do curso
+                </Tabs.Content>
+            </Tabs.Root>
+
         </div>
     );
 };
